6af7cee2089e5ce9bc1ab5025e8ceee7e9314023
Morven Lewis-Everley <morven@i-lateral.com>
2010-02-24 10:28:10 +0000

    Add check to ensure sql friendly menu slug

--- code/CustomMenu.php
+++ code/CustomMenu.php
@@ -1,6 +1,7 @@
 <?php
 class CustomMenu extends Extension {
 	public function CustomMenu($menu = null) {
+		$menu = Convert::raw2sql($menu);
 		if($menu) {
 			if(DataObject::get_one('CustomMenuHolder',"Slug = '$menu'")) {
 				$result = DataObject::get_one('CustomMenuHolder',"Slug = '$menu'")->Pages();
