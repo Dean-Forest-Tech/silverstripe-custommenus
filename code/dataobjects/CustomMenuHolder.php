461525bf3dcd51357f814cad83153d7b87d31808
Morven Lewis-Everley <morven@i-lateral.com>
2010-02-25 17:04:46 +0000

    Attempt to fix wierd cms sitetree issues

--- code/dataobjects/CustomMenuHolder.php
+++ code/dataobjects/CustomMenuHolder.php
@@ -10,44 +10,48 @@ class CustomMenuHolder extends DataObject {
 	);
 	
 	/**
-	 * Create default menu items
+	 * Create default menu items - Currently disabled due to issues with
+	 * 2.4 beta 1.
 	 * 
 	 * @see sapphire/core/model/DataObject#requireDefaultRecords()
 	 */
+	/*
 	function requireDefaultRecords() {
 		parent::requireDefaultRecords();
 		
 		// Main Menu
-		if(!DataObject::get("CustomMenuHolder")) {
-			$menu = new CustomMenuHolder();
-			$menu->Title = 'Main Menu';
-			$menu->Slug = "main-menu";
-			$menu->write();
-			$menu->flushCache();
-			if(method_exists('Database','alteration_message'))
-				Database::alteration_message("Main menu created","created");
-			else
-				DB::alteration_message("Main menu created","created");
-			
-			$menu = new CustomMenuHolder();
-			$menu->Title = 'Header Menu';
-			$menu->Slug = "header-menu";
-			$menu->write();
-			$menu->flushCache();
-			if(method_exists('Database','alteration_message'))
-				Database::alteration_message("Header menu created","created");
-			else
-				DB::alteration_message("Header menu created","created");
-			
-			$menu = new CustomMenuHolder();
-			$menu->Title = 'Footer Menu';
-			$menu->Slug = "footer-menu";
-			$menu->write();
-			$menu->flushCache();
-			if(method_exists('Database','alteration_message'))
-				Database::alteration_message("Footer menu created","created");
-			else
-				DB::alteration_message("Footer menu created","created");
+		if($this->class == 'CustomMenuHolder') {
+		    if(!DataObject::get_one($this->class)) {
+			    $menu = new CustomMenuHolder();
+			    $menu->Title = 'Main Menu';
+			    $menu->Slug = "main-menu";
+			    $menu->write();
+			    $menu->flushCache();
+			    if(method_exists('Database','alteration_message'))
+				    Database::alteration_message("Main menu created","created");
+			    else
+				    DB::alteration_message("Main menu created","created");
+
+			    $menu = new CustomMenuHolder();
+			    $menu->Title = 'Header Menu';
+			    $menu->Slug = "header-menu";
+			    $menu->write();
+			    $menu->flushCache();
+			    if(method_exists('Database','alteration_message'))
+				    Database::alteration_message("Header menu created","created");
+			    else
+				    DB::alteration_message("Header menu created","created");
+
+			    $menu = new CustomMenuHolder();
+			    $menu->Title = 'Footer Menu';
+			    $menu->Slug = "footer-menu";
+			    $menu->write();
+			    $menu->flushCache();
+			    if(method_exists('Database','alteration_message'))
+				    Database::alteration_message("Footer menu created","created");
+			    else
+				    DB::alteration_message("Footer menu created","created");
+		    }
 		}
-	}
+	}*/
 }
--- templates/CustomMenuAdmin_left.ss
+++ templates/CustomMenuAdmin_left.ss
@@ -1,23 +1,16 @@
 <div id="treepanes" style="overflow-y: auto;">
-	<h2 id="heading_sitetree" class=" selected title"><% _t('CUSTOMMENUS.LEFTTITLE','Custom Menus') %></h2>
-	
+	<h2 id="heading_sitetree" class="selected title"><% _t('CUSTOMMENUS.LEFTTITLE','Custom Menus') %></h2>
+	$LeftMenuForm
+	<% if get_menus %>
 	<div id="sitetree_holder">
-		$LeftMenuForm
-		
-		<div id="sitetree_ul">
-			<% if get_menus %>
-			<ul id="sitetree" class="tree"> 
-				<li id="record-0" class="Root nodelete last">
-					<span class="a Root nodelete last">
-                                            <span class="b"><span class="c"><a href="admin/menus/root">Menus</a></span></span>
-                                        </span>
-					<ul>
-						<% control get_menus %><li id="record-$ID" class="$ClassName $Current">
-                                                   <a href="admin/menus/show/$ID">$Title</a>
-						</li><% end_control %>
-					</ul>
-				</li>
-			</ul><% end_if %>
-		</div>
-	</div>
+	    <ul id="sitetree" class="tree">
+		    <li id="record-root" class="Root last">
+			<a href="admin/menus/root">Menus</a>
+			    <ul>
+				<% control get_menus %><li id="record-$ID" class="Menu"><a href="admin/menus/show/$ID">$Title</a></li><% end_control %>
+			    </ul>
+		    </li>
+	    </ul>
+	</div><% end_if %>
+
 </div>
