ba84569dd80326dd8a27fea4d5a85a5f2fda0b2f
Morven Lewis-Everley <morven@i-lateral.com>
2018-01-19 18:56:56 +0000

    Remove subsites code

--- _config.php
+++ _config.php
@@ -1,7 +1,1 @@
 <?php
-
-// Enable Subsite Support if needed
-if(class_exists('Subsite')) {
-    CustomMenuHolder::add_extension('CustomMenuHolder_SubsiteExtension');
-    CustomMenuAdmin::add_extension('SubsiteMenuExtension');
-}
