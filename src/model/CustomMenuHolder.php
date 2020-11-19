1278cfa8ede7c4c19102ed039e5780cc803a2b44
Makreig <mark@i-lateral.com>
2020-11-19 14:45:57 +0000

    removed obsolete message calls

--- src/model/CustomMenuHolder.php
+++ src/model/CustomMenuHolder.php
@@ -98,33 +98,21 @@ class CustomMenuHolder extends DataObject implements PermissionProvider
             $menu->Slug = "main-menu";
             $menu->write();
             $menu->flushCache();
-            if (method_exists('Database', 'alteration_message')) {
-                Database::alteration_message("Main menu created", "created");
-            } else {
-                DB::alteration_message("Main menu created", "created");
-            }
+            DB::alteration_message("Main menu created", "created");
 
             $menu = new CustomMenuHolder();
             $menu->Title = 'Header Menu';
             $menu->Slug = "header-menu";
             $menu->write();
             $menu->flushCache();
-            if (method_exists('Database', 'alteration_message')) {
-                Database::alteration_message("Header menu created", "created");
-            } else {
-                DB::alteration_message("Header menu created", "created");
-            }
+            DB::alteration_message("Header menu created", "created");
 
             $menu = new CustomMenuHolder();
             $menu->Title = 'Footer Menu';
             $menu->Slug = "footer-menu";
             $menu->write();
             $menu->flushCache();
-            if (method_exists('Database', 'alteration_message')) {
-                Database::alteration_message("Footer menu created", "created");
-            } else {
-                DB::alteration_message("Footer menu created", "created");
-            }
+            DB::alteration_message("Footer menu created", "created");
         }
 
             
